7471707ad3f87af6b6a1be087cd40c7f59405051
Enrique Golpe <enriquegolpe@hotmail.com>
2022-08-03 23:08:24 +0200

    ejercicios3_4_5

---
+++ ejercicios3_4_5/ejercicios3_4_5/Program.cs
@@ -0,0 +1,43 @@
+﻿//Ejercicio 3
+/*
+Console.WriteLine("Escribe tu nombre");
+String nombre = Console.ReadLine();
+Console.WriteLine("Escribe tu apellido");
+String apellido = Console.ReadLine();
+Console.WriteLine("Escribe tu edad");
+String edad = Console.ReadLine();
+int edad2 = Convert.ToInt32(edad);
+Console.WriteLine("¿Sabes programar?");
+String sabeProgramar = Console.ReadLine();
+Console.WriteLine(nombre + " " + apellido + " " + "tiene " + edad + " años" + " " + "¿sabe programar? " + sabeProgramar);
+
+//Ejercicio 4
+
+//Coche
+
+int puertas;
+int ruedas;
+String marca;
+bool ITV;
+
+//Mesa
+
+float peso;
+float largo;
+String material;
+String color;
+
+
+//Ejercicio 5
+
+int num = 20;
+
+Console.WriteLine(num >= 18);
+
+char a = 'a';
+*/
+int b = 10;
+int c = 5;
+
+Console.WriteLine(c < b && b > c);
+Console.WriteLine(b < c || c < b);
