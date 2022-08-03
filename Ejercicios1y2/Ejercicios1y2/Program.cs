701b1b585208ec31c53c942c657d9544f3a8c398
Enrique Golpe <enriquegolpe@hotmail.com>
2022-08-03 20:21:42 +0200

    first commit

---
+++ Ejercicios1y2/Ejercicios1y2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Escribe tu nombre:");
+String nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+
+
+String text1 = DateTime.Now.ToLongTimeString();
+Console.WriteLine(text1);
+
